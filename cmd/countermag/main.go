7e96347492025123066ca98bd10a684cfb1018d2
Constantin Barac <ilconstantinbarac@gmail.com>
2024-09-26 13:50:21 +0300

    Make sure server shuts down gracefully

--- cmd/countermag/main.go
+++ cmd/countermag/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func handleTest(w http.ResponseWriter, r *http.Request) {
@@ -21,12 +25,34 @@ func handleTest(w http.ResponseWriter, r *http.Request) {
 func main() {
 	handler := http.NewServeMux()
 	handler.HandleFunc("/test", handleTest)
-	
 	server := http.Server{
 		Addr: ":8080",
 		Handler: handler,
 	}
+	
+	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
 
-	fmt.Printf("Listening on port 8080...\n")
-	server.ListenAndServe()
+	go func() {
+		fmt.Printf("App server listening on %s\n", server.Addr)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("error listening and serving: %s\n", err)
+		}
+
+		fmt.Printf("Stopped serving new connections\n")
+	}()
+
+	<-ctx.Done()
+
+	fmt.Printf("Shutting down application server...\n")
+
+	shutdownCtx, cancel := context.WithTimeout(ctx, 5 * time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		fmt.Printf("Error shutting down application server: %s\n", err)
+	}
+
+	fmt.Printf("Shutdown complete\n")
 }
