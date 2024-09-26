afd23035065c25753e79f0e11c4b9e9decbcd8bf
Constantin Barac <ilconstantinbarac@gmail.com>
2024-09-26 15:34:59 +0300

    Use test-dedicated fn for new database

--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -6,24 +6,32 @@ import (
 	"testing"
 )
 
+func getTestDatabase() *Database {
+	return &Database{
+		ctx: context.Background(),
+		logger: slog.Default(),
+		persister: &MockPersister{},
+	}
+}
+
 func TestDatabaseUnusedWords(t *testing.T) {
-	d := NewDatabase(context.Background(), slog.Default(), &MockPersister{})
-	
+	d := getTestDatabase()
+
 	d.AddOccurences("hello", 1)
-	
+
 	if d.Get("world") != 0 {
 		t.Error("expected 0, got", d.Get("world"))
 	}
 }
 
 func TestDatabaseSetAndGet(t *testing.T) {
-	d := NewDatabase(context.Background(), slog.Default(), &MockPersister{})
-	
+	d := getTestDatabase()
+
 	d.AddOccurences("hello", 1)
 	d.AddOccurences("hello", 2)
 	d.AddOccurences("hello", 3)
 	d.AddOccurences("hello", 4)
-	
+
 	if d.Get("hello") != 10 {
 		t.Error("expected 10, got", d.Get("hello"))
 	}
@@ -31,26 +39,26 @@ func TestDatabaseSetAndGet(t *testing.T) {
 
 func TestDatabaseSaveLoadSnapshot(t *testing.T) {
 	persister := MockPersister{}
-	d := NewDatabase(context.Background(), slog.Default(), &persister)
-	
+	d := getTestDatabase()
+
 	d.AddOccurences("hello", 1)
 	d.AddOccurences("hello", 2)
 	d.AddOccurences("hello", 3)
 	d.AddOccurences("hello", 4)
-	
+
 	if err := d.SaveSnapshot(); err != nil {
 		t.Error(err)
 	}
-	
+
 	if persister.data["hello"] != 10 {
 		t.Error("[PERSISTER] expected 10, got", persister.data["hello"])
 	}
 
-	recoveredDatabase := NewDatabase(context.Background(), slog.Default(), &persister)
+	recoveredDatabase := getTestDatabase()
 	if err := recoveredDatabase.LoadSnapshot(); err != nil {
 		t.Error(err)
 	}
-	
+
 	if recoveredDatabase.Get("hello") != 10 {
 		t.Error("expected 10, got", d.Get("hello"))
 	}
