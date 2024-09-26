e8e210e70a13d41ea927a7204fba641f7b121f6a
Constantin Barac <ilconstantinbarac@gmail.com>
2024-09-26 13:40:54 +0300

    Add http server + test route

--- cmd/countermag/main.go
+++ cmd/countermag/main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 )
 
+func handleTest(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Test route called")
+
+	testResponse := map[string]string{
+		"message": "good test",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(testResponse)
+}
+
 func main() {
-	fmt.Println("Hello, World!")	
+	handler := http.NewServeMux()
+	handler.HandleFunc("/test", handleTest)
+	
+	server := http.Server{
+		Addr: ":8080",
+		Handler: handler,
+	}
+
+	fmt.Printf("Listening on port 8080...\n")
+	server.ListenAndServe()
 }
