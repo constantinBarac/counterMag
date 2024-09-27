92241f70b3da55cab19a6756559e9555ecb91f72
Constantin Barac <ilconstantinbarac@gmail.com>
2024-09-27 19:04:37 +0300

    Add more logs to scripts

--- scripts/load.sh
+++ scripts/load.sh
@@ -6,6 +6,8 @@ RESULTS_DIR=$ROOT/out/tests/load/results
 
 $SCRIPT_DIR/spinup_bg.sh 
 
-locust -f $ROOT/tests/load/analyze_and_query.py --headless --host http://127.0.0.1:8100 -u 200 -r 20 --run-time 10 --csv $RESULTS_DIR   
+echo "Starting load test..."
+python3 -m locust -f $ROOT/tests/load/analyze_and_query.py --headless --host http://127.0.0.1:8100 -u 200 -r 200 --run-time 10 --csv $RESULTS_DIR   
+echo "Load test done"
 
 $SCRIPT_DIR/teardown.sh 
--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "reks"
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROOT=$SCRIPT_DIR/.. 
 
